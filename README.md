14a2835236dabd68718bde88da3d9c09750b04d3
Mihir Patel <29385964+Mhr85@users.noreply.github.com>
2019-07-16 14:08:10 -0400

    finished the project

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# friendsFinder
+# Friend Finder
+
+Find a Friend in one easy step.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Installing
+
+Clone the repo
+```
+$ git clone https://github.com/Mhr85/friendsFinder.git
+
+```
+Install dependencies
+
+```
+$ npm i
+
+```
+
+Run the server
+
+```
+$ node server.js
+
+```
+
+### Deployed Version 
+
+![Main Page](app/public/assets/images/Mainpage.png)
+
+
+Heroku Link: 
---
+++ app/data/friends.js
@@ -0,0 +1,66 @@
+var friends = [
+    {
+        "name":"Miki",
+        "photo":"../../public/assets/images/Miki.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }, {
+        "name":"Stephany",
+        "photo":"../../public/assets/images/Stephany.jpg",
+        "scores":[
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    }, {
+        "name":"John",
+        "photo":"../../public/assets/images/John.jpg",
+        "scores":[
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+      ]
+    }, {
+        "name":"Mark",
+        "photo":"../../public/assets/images/Mark.jpg",
+        "scores":[
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+      ]
+    } 
+];
+    
+
+  module.exports = friends;
